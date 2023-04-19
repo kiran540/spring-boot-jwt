49f5401a1fd9b5f2a7c4e9ce2eb25fa8b3d52186
kishore457 <kishoresmashes@gmail.com>
2023-04-19 13:15:59 +0530

    added ModelMapper

--- src/main/java/com/techkernal/springbootjwt/SpringbootjwtApplication.java
+++ src/main/java/com/techkernal/springbootjwt/SpringbootjwtApplication.java
@@ -1,13 +1,20 @@
 package com.techkernal.springbootjwt;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringbootjwtApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootjwtApplication.class, args);
+	}
+
+	@Bean
+	ModelMapper modelMapper(){
+		return new ModelMapper();
 	}
 
 }
